6c954057316e317f3f08f1e483b5177af06dde12
Ray Amable <rayamable@gmail.com>
2020-02-04 16:38:43 +0000

    add doc controller and routes

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,31 @@
+class DocsController < ApplicationController
+
+	def index
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	def create
+	end
+
+	def new
+	end
+
+	private
+
+	def find_doc
+	end
+
+	def doc_params
+	end
+end
